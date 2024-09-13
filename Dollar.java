107f77e5b5d65eb65c40f54b050003726ee17382
Carlos Michael <carlosmaykesilva@gmail.com>
2024-09-13 19:44:28 -0300

    Terceira alteração

--- Dollar.java
+++ Dollar.java
@@ -3,15 +3,15 @@ class Dollar {
     Dollar(int amount) {
         this.amount= amount;
     }
-    Dollar times(int multiplier) {
+    Money times(int multiplier)  {
         return new Dollar(amount * multiplier);
     }
 }		
 
-public void testMultiplication() {
-    Dollar five = new Dollar(5);
-    assertEquals(new Dollar(10), five.times(2));
-    assertEquals(new Dollar(15), five.times(3));
+public void testFrancMultiplication() {
+    Money five = Money.franc(5);
+    assertEquals(Money.franc(10), five.times(2));
+    assertEquals(Money.franc(15), five.times(3));
 }
 
 class Franc {   
@@ -19,20 +19,27 @@ class Franc {
     Franc(int amount) {      
         this.amount= amount;
     }					
-    Franc times(int multiplier)  {      
-        return new Franc(amount * multiplier);					
-    }   
+    Money times(int multiplier)  {
+        return new Franc(amount * multiplier);
+    }  
     public boolean equals(Object object) {					
         Franc franc = (Franc) object;      
         return amount == franc.amount;					
     }					
 }
 
+public void testMultiplication() {
+    Money five = Money.dollar(5);
+    assertEquals(Money.dollar(10), five.times(2));
+    assertEquals(Money.dollar(15), five.times(3));
+}
+
 public void testEquality() {
-    assertTrue(new Dollar(5).equals(new Dollar(5)));
-    assertFalse(new Dollar(5).equals(new Dollar(6)));
-    assertTrue(new Franc(5).equals(new Franc(5)));
-    assertFalse(new Franc(5).equals(new Franc(6)));
+    assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+    assertTrue(Money.franc(5).equals(Money.franc(5)));
+    assertFalse(Money.franc(5).equals(Money.franc(6)));
+    assertFalse(Money.franc(5).equals(Money.dollar(5)));
 }
 
 public boolean equals(Object object)  {
@@ -42,9 +49,19 @@ public boolean equals(Object object)  {
 
 class Money  {
     protected int amount;
+
+    static Money dollar(int amount)  {
+        return new Dollar(amount);
+    }
     
-    public boolean equals(Object object)  {
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
+    
+    abstract Money times(int multiplier);
+    
+    public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && getClass().equals(money.getClass());
     }   
 }
