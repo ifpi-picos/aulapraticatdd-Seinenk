9c6d4cd43ae70ebb8a3e9c47c5b30a6f08d3c234
Carlos Michael <carlosmaykesilva@gmail.com>
2024-09-13 19:39:09 -0300

    Segunda alteração

--- Dollar.java
+++ Dollar.java
@@ -1,11 +1,50 @@
 class Dollar {
-    int amount = 10;
-    Dollar(int amount) {}			
-    void times(int multiplier) {}
-}	
+    private int amount;
+    Dollar(int amount) {
+        this.amount= amount;
+    }
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}		
 
 public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
+}
+
+class Franc {   
+    private int amount;					
+    Franc(int amount) {      
+        this.amount= amount;
+    }					
+    Franc times(int multiplier)  {      
+        return new Franc(amount * multiplier);					
+    }   
+    public boolean equals(Object object) {					
+        Franc franc = (Franc) object;      
+        return amount == franc.amount;					
+    }					
+}
+
+public void testEquality() {
+    assertTrue(new Dollar(5).equals(new Dollar(5)));
+    assertFalse(new Dollar(5).equals(new Dollar(6)));
+    assertTrue(new Franc(5).equals(new Franc(5)));
+    assertFalse(new Franc(5).equals(new Franc(6)));
+}
+
+public boolean equals(Object object)  {
+    Dollar dollar = (Dollar) object;
+    return amount == dollar.amount;
+}
+
+class Money  {
+    protected int amount;
+    
+    public boolean equals(Object object)  {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }   
 }
